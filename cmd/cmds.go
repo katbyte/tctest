c5ab5ab5292129858be142d63b3019f5653cfd41
kt <kt@katbyte.me>
2019-05-10 11:56:45 -0700

    updated pass flag warning

--- cmd/cmds.go
+++ cmd/cmds.go
@@ -128,7 +128,7 @@ Complete documentation is available at https://github.com/katbyte/tctest`,
 	pflags.StringVarP(&flags.TC.ServerUrl, "server", "s", "", "the TeamCity server's ur;")
 	pflags.StringVarP(&flags.TC.BuildTypeId, "buildtypeid", "b", "", "the TeamCity BuildTypeId to trigger")
 	pflags.StringVarP(&flags.TC.User, "user", "u", "", "the TeamCity user to use")
-	pflags.StringVarP(&flags.TC.Pass, "pass", "p", "", "the TeamCity password to use (unless you know what your doing don't use this!)")
+	pflags.StringVarP(&flags.TC.Pass, "pass", "p", "", "the TeamCity password to use (consider exporting pass to TCTEST_PASS instead)")
 
 	pflags.StringVarP(&flags.PR.Repo, "repo", "r", "", "repository the pr resides in, such as `terraform-providers/terraform-provider-azurerm`")
 	pflags.StringVarP(&flags.PR.FileRegEx, "fileregex", "", "(^[a-z]*/resource_|^[a-z]*/data_source_)", "the regex to filter files by`")
