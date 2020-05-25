926061d6bc51af4648b671a3e4fc9005e8c29e83
kt <kt@katbyte.me>
2020-05-25 13:19:55 -0700

    make fmt

--- common/http.go
+++ common/http.go
@@ -12,7 +12,7 @@ var HTTP = http.DefaultClient
 
 func HttpGetReader(url string) (*io.ReadCloser, error) {
 	Log.Debug("HTTP GET: " + url)
-	
+
 	resp, err := HTTP.Get(url)
 	if err != nil {
 		return nil, fmt.Errorf("HTTP error: %v", err)
