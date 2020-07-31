06c38e174bc7ffcdcc92e5140b9df94531742e86
katbyte <katbyte@TeamCity>
2020-07-31 18:26:09 +0000

    TeamCity change in 'tctest' project: build features of 'build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
@@ -48,6 +49,18 @@ object build : BuildType({
 
     triggers {
         vcs {
+        }
+    }
+
+    features {
+        commitStatusPublisher {
+            vcsRootExtId = "${DslContext.settingsRoot.id}"
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = personalToken {
+                    token = "credentialsJSON:f8789b57-76c2-4c9b-b8df-8de0d5cddf89"
+                }
+            }
         }
     }
 })
