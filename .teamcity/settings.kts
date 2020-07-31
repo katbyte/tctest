9cc1063bf19a1ab9d126a1c9dcf3f89f046a2982
kt <kt@katbyte.me>
2020-07-31 11:54:25 -0700

    add pull request feature

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,28 +3,6 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPu
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
 version = "2020.1"
 
 project {
@@ -58,6 +36,16 @@ object build : BuildType({
     }
 
     features {
+        pullRequests {
+            vcsRootExtId = "${DslContext.settingsRoot.id}"
+            provider = github {
+                authType = token {
+                    token = "credentialsJSON:f8789b57-76c2-4c9b-b8df-8de0d5cddf89"
+                }
+                filterAuthorRole = PullRequests.GitHubRoleFilter.EVERYBODY
+            }
+        }
+
         commitStatusPublisher {
             vcsRootExtId = "${DslContext.settingsRoot.id}"
             publisher = github {
@@ -95,6 +83,16 @@ object lint : BuildType({
     }
 
     features {
+            pullRequests {
+                vcsRootExtId = "${DslContext.settingsRoot.id}"
+                provider = github {
+                    authType = token {
+                        token = "credentialsJSON:f8789b57-76c2-4c9b-b8df-8de0d5cddf89"
+                    }
+                    filterAuthorRole = PullRequests.GitHubRoleFilter.EVERYBODY
+                }
+            }
+
             commitStatusPublisher {
                 vcsRootExtId = "${DslContext.settingsRoot.id}"
                 publisher = github {
