a78e10952456582a8b9f67ecb37f61af17f8d5d9
Steph <steph@hashicorp.com>
2021-11-19 11:47:07 +0100

    add skip-queue flag

--- cmd/tctest/cli/cmds.go
+++ cmd/tctest/cli/cmds.go
@@ -36,12 +36,17 @@ type WaitFlags struct {
 	RunTimeout   int
 }
 
+//type TriggerFlags struct {
+//	QueueAtTop bool
+//}
+
 type FlagData struct {
 	TC                  TCFlags
 	PR                  PRFlags
 	Wait                WaitFlags
 	ServicePackagesMode bool
 	AllTests            bool
+	SkipQueue			bool
 }
 
 func ValidateParams(params []string) func(cmd *cobra.Command, args []string) error {
@@ -105,8 +110,9 @@ Complete documentation is available at https://github.com/katbyte/tctest`,
 			buildTypeId := viper.GetString("buildtypeid")
 			properties := viper.GetString("properties")
 			wait := viper.GetBool("wait")
+			skipQueue := viper.GetBool("skip-queue")
 
-			return NewTeamCityFromViper().BuildCmd(buildTypeId, properties, branch, "", testRegEx, wait)
+			return NewTeamCityFromViper().BuildCmd(buildTypeId, properties, branch, "", testRegEx, wait, skipQueue)
 		},
 	}
 	root.AddCommand(branch)
@@ -179,8 +185,9 @@ Complete documentation is available at https://github.com/katbyte/tctest`,
 					branch := fmt.Sprintf("refs/pull/%s/merge", pr)
 					properties := viper.GetString("properties")
 					wait := viper.GetBool("wait")
+					skipQueue := viper.GetBool("skip-queue")
 
-					if err := NewTeamCityFromViper().BuildCmd(buildTypeId, properties, branch, testRegEx, serviceInfo, wait); err != nil {
+					if err := NewTeamCityFromViper().BuildCmd(buildTypeId, properties, branch, testRegEx, serviceInfo, wait, skipQueue); err != nil {
 						return err
 					}
 					fmt.Println()
@@ -275,6 +282,8 @@ Complete documentation is available at https://github.com/katbyte/tctest`,
 	pflags.IntVarP(&flags.Wait.QueueTimeout, "queue-timeout", "", 60, "How long to wait for a queued build to start running before tctest times out")
 	pflags.IntVarP(&flags.Wait.RunTimeout, "run-timeout", "", 60, "How long to wait for a running build to finish before tctest times out")
 
+	pflags.BoolVarP(&flags.SkipQueue, "skip-queue", "", false, "Put the build to the queue top")
+
 	// binding map for viper/pflag -> env
 	m := map[string]string{
 		"server":        "TCTEST_SERVER",
@@ -292,6 +301,7 @@ Complete documentation is available at https://github.com/katbyte/tctest`,
 		"queue-timeout": "",
 		"run-timeout":   "",
 		"latest":        "TCTEST_LATESTBUILD",
+		"skip-queue":  "",
 	}
 
 	for name, env := range m {
