01233dd8bc7d151e7e002dacef88a018567f82fa
kt <kt@katbyte.me>
2025-01-28 07:18:46 -0800

    Update .github/images/set_env_example.sh

--- .github/images/set_env_example.sh
+++ .github/images/set_env_example.sh
@@ -4,7 +4,5 @@ export TCTEST_SERVER="ci.katbyte.me"
 export TCTEST_TOKEN="abc123"
 export TCTEST_REPO="terraform-providers/terraform-provider-azurerm"
 
-# The build type ID, or build configuration ID, can be found in the Teamcity
-# website, in the URL path after "/buildConfiguration/"
-# https://mycompany.teamcity.com/buildConfiguration/<THIS_IS_THE_ID>#all-projects
+# The build type ID, or build configuration ID, can be found in the Teamcity website, in the URL path after "/buildConfiguration/"  https://mycompany.teamcity.com/buildConfiguration/<THIS_IS_THE_ID>#all-projects
 export TCTEST_BUILDTYPEID="TCBuildTypeId"
